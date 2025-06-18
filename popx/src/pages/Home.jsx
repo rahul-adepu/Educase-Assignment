c71987f8a7a61fc7733ee8697c8b6b194408fade
rahul-adepu <rahuladepu002@gmail.com>
2025-06-18 15:15:51 +0530

    Initialize React 18 + Vite project with Tailwind CSS v3.4.17 setup

---
+++ popx/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <button className="bg-purple-600 text-white px-4 py-2 rounded">
+      Create Account
+    </button>
+  );
+}
+
+export default Home;
