ad055e57bf97d7f91c7c02a57ef73b9c5a5bec07
rahul-adepu <rahuladepu002@gmail.com>
2025-06-18 12:52:25 +0530

    Done with connecting Database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 require("dotenv").config();
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("home page");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running");
+app.listen(process.env.PORT, async () => {
+  try {
+    await connectDB();
+    console.log("Database Connected!!!");
+    console.log(`Server is running on port ${process.env.PORT}`);
+  } catch (err) {
+    console.error("Failed to connect to the database", err);
+  }
 });
